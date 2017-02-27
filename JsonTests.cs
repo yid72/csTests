d9ffb31c7f3aeedf46c2c1f5c4a930ea884a6462
dyd <duyidong@gmail.com>
2017-02-26 17:30:00 -0800

    second commit

---
+++ JsonTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using cs_Tests.Models;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace cs_Tests
+{
+    [TestClass]
+    public class JsonTests
+    {
+        [TestMethod]
+        public void TestOutput()
+        {
+            var model = new HelloWorldModel();
+            model.Hello = "Hello World";
+            var ser = new DataContractJsonSerializer(typeof(HelloWorldModel));
+            var stream = new MemoryStream();
+            ser.WriteObject(stream, model);
+            stream.Position = 0;
+            var reader = new StreamReader(stream);
+            Console.WriteLine(reader.ReadToEnd());
+        }
+    }
+}
---
+++ Models/HelloWorldModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace cs_Tests.Models
+{
+    [DataContract]
+    class HelloWorldModel
+    {
+        private string hello = "Hello";
+
+        [DataMember]
+        public string Hello
+        {
+            get { return hello; }
+
+            set { hello = value; }
+        }
+    }
+}

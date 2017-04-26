c4131dc407e54fbf2fc3a14853ea2ddd113779e2
dyd <duyidong@gmail.com>
2017-04-25 18:00:41 -0700

    url tests

--- Misc/UrlTest.cs
+++ Misc/UrlTest.cs
@@ -8,6 +8,27 @@ namespace cs_Tests.Misc
     [TestClass]
     public class UrlTest
     {
+        private const string TesterEmail = "tester@microsoft.com";
+        private const string HelloWorld = "hello world";
+        private const string HelloWorld1 = "hello+world";
+        private string testBaseUrl = "http://www.foo.com/foo.aspx";
+
+        [TestMethod]
+        public void TestUrlBuilder()
+        {
+            var uriBuilder = new UriBuilder(this.testBaseUrl);
+            Console.WriteLine(uriBuilder.ToString());
+            Console.WriteLine(uriBuilder.Query.ToString());
+        }
+
+        [TestMethod]
+        public void TestEscapeDataString()
+        {
+            Console.WriteLine(HttpUtility.UrlEncode(HelloWorld));
+            Console.WriteLine(Uri.EscapeUriString(HelloWorld));
+            Console.WriteLine(Uri.EscapeDataString(HelloWorld));
+        }
+
         [TestMethod]
         public void TestGetLeftPart()
         {
@@ -17,12 +38,65 @@ namespace cs_Tests.Misc
         }
 
         [TestMethod]
-        public void TestQueryParameters()
+        public void TestBuildQuery()
         {
             NameValueCollection p = HttpUtility.ParseQueryString(String.Empty);
-            p["foo"] = "hello";
-            p["bar"] = "world  good";
+            p["email"] = TesterEmail;
+            p["value"] = HelloWorld;
+
+            Assert.AreEqual("email=tester%40microsoft.com&value=hello+world", p.ToString());
+        }
+
+        [TestMethod]
+        public void TestBuildQuery1()
+        {
+            NameValueCollection p = HttpUtility.ParseQueryString(String.Empty);
+            p["email"] = TesterEmail;
+            p["value"] = HelloWorld1;
+
             Console.WriteLine(p.ToString());
+            Assert.AreEqual("email=tester%40microsoft.com&value=hello%2bworld", p.ToString());
+        }
+
+        [TestMethod]
+        public void TestParseQuery()
+        {
+            NameValueCollection p = HttpUtility.ParseQueryString("email=tester%40microsoft.com&value=hello+world");
+            Assert.AreEqual(TesterEmail, p["email"]);
+            Assert.AreEqual(HelloWorld, p["value"]);
+        }
+
+        [TestMethod]
+        public void TestParseQuery1()
+        {
+            NameValueCollection p = HttpUtility.ParseQueryString("email=tester%40microsoft.com&value=hello%2bworld");
+            Assert.AreEqual(TesterEmail, p["email"]);
+            Assert.AreEqual(HelloWorld1, p["value"]);
+        }
+
+
+        [TestMethod]
+        public void TestParseQueryParameters2()
+        {
+            NameValueCollection p = HttpUtility.ParseQueryString(String.Empty);
+            p["email"] = Uri.EscapeDataString("admin@prepspo.msolctp-int.com");
+            p["key"] = Uri.EscapeDataString("let's say hello+world");
+
+            string escaped = p.ToString();
+            Console.WriteLine("Escaped url: " + escaped);
+
+            string unescaped = HttpUtility.UrlDecode(escaped);
+
+            NameValueCollection p1 = HttpUtility.ParseQueryString(unescaped);
+            Console.WriteLine("email: " + p1["email"]);
+            Console.WriteLine("key: " + p1["key"]);
+        }
+
+        public void TestQueryParametersBad()
+        {
+            NameValueCollection p2 = HttpUtility.ParseQueryString("email=admin@prepspo.msolctp-int.com&key=hello+world");
+            Console.WriteLine("email: " + p2["email"]);
+            Console.WriteLine("key: " + p2["key"]);
         }
     }
 }

599f9eb923251eac0781a0d23f3a2e855210da90
Yidong Du <yid@microsoft.com>
2017-06-02 15:22:45 -0700

    refactor httputility tests

--- Web/HttpUtilityTest.cs
+++ Web/HttpUtilityTest.cs
@@ -10,15 +10,22 @@ namespace cs_Tests.Web
         private const string Hello = @"hello world""'%<+/";
         private const string SpecialPath = "http://www.foo.com/hello world%#.txt";
 
-
         [TestMethod]
-        public void TestUrlEncode()
+        public void TestHttpUtilityUrlEncode()
         {
-            Console.WriteLine(HttpUtility.UrlEncode(SpecialPath));
+            // All researved characters are escaped. The space is converted to +
+            Assert.AreEqual("http%3a%2f%2fwww.foo.com%2fhello+world%25%23.txt", HttpUtility.UrlEncode(SpecialPath));
         }
 
         [TestMethod]
-        public void TestHtmlEncode()
+        public void TestHttpUtilityUrlPathEncode()
+        {
+            // The URI reserved characters are kept as is. The space is converted to %20
+            Assert.AreEqual("http://www.foo.com/hello%20world%#.txt", HttpUtility.UrlPathEncode(SpecialPath));
+        }
+
+        [TestMethod]
+        public void TestHttpUtilityHtmlEncode()
         {
             Assert.AreEqual("&quot;&lt;&gt;&amp;", HttpUtility.HtmlEncode(@"""<>&"));
             Console.WriteLine(HttpUtility.HtmlEncode(Hello));
